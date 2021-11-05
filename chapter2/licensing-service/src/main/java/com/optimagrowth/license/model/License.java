5b57131fc10668adfc247a00f6861385788db732
Cristian Linux <csdavidg@hotmail.com>
2021-11-04 19:36:26 -0500

    Adding repository The properties are being pulled from a gitHub repository using the Spring config project

--- chapter2/licensing-service/src/main/java/com/optimagrowth/license/model/License.java
+++ chapter2/licensing-service/src/main/java/com/optimagrowth/license/model/License.java
@@ -5,7 +5,13 @@ import lombok.Setter;
 import lombok.ToString;
 import org.springframework.hateoas.RepresentationModel;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+
+/*
 @Getter @Setter @ToString
 public class License extends RepresentationModel<License> {
 
@@ -16,4 +22,30 @@ public class License extends RepresentationModel<License> {
 	private String productName;
 	private String licenseType;
 
+}*/
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "licenses")
+public class License extends RepresentationModel<License> {
+
+    @Id
+    @Column(name = "license_id", nullable = false)
+    private String licenseId;
+    private String description;
+    @Column(name = "organization_id", nullable = false)
+    private String organizationId;
+    @Column(name = "product_name", nullable = false)
+    private String productName;
+    @Column(name = "license_type", nullable = false)
+    private String licenseType;
+    @Column(name = "comment")
+    private String comment;
+
+    public License withComment(String comment) {
+        this.setComment(comment);
+        return this;
+    }
 }
